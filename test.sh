abb809af0ed4cd4bf7ba8fa3564e961afaf6997d
CrossDvs <activityscuslt52i@outlook.com>
2025-02-16 09:33:03 -0300

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Exibe uma mensagem de boas-vindas
+echo "Olá, mundo! Este é um script simples."
+
+# Mostra a data e hora atual
+echo "A data e hora atual é: $(date)"
+
+# Exibe a lista de arquivos no diretório atual
+echo "Arquivos no diretório atual:"
+ls￼Enter
