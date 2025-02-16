ecc29e1c4077f7531132bbee6fe12a4712bddfb5
CrossDvs <activityscuslt52i@outlook.com>
2025-02-16 09:48:43 -0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 
-# Exibe uma mensagem de boas-vindas
-echo "Olá, mundo! Este é um script simples."
-
-# Mostra a data e hora atual
-echo "A data e hora atual é: $(date)"
-
-# Exibe a lista de arquivos no diretório atual
-echo "Arquivos no diretório atual:"
-ls￼Enter
+adb -s emulator-5554 shell id
